82ff936cf9cd65b227a472e5080e27205acd9b99
novoville <vagelis@novoville.com>
2021-04-24 15:02:05 +0300

    animate image on content

--- resources/views/site/blocks/image.blade.php
+++ resources/views/site/blocks/image.blade.php
@@ -1,3 +1,3 @@
-<div class="box-post-width content-1140 center-relative top-20 image-100">
+<div class="box-post-width content-1140 center-relative top-20 image-100 animate">
     <img src="{!! $block->image('image','desktop') !!}" alt="" />
 </div>
--- resources/views/site/blocks/title_image_text.blade.php
+++ resources/views/site/blocks/title_image_text.blade.php
@@ -1,7 +1,7 @@
 <div class="entry-content top-20 animate image-100">
     <h3>{!! $block->translatedInput('title') !!}</h3>
 
-    <img style="max-width: 100%" class="lozad" data-src="{!! $block->image('image','flexible') !!}" alt="" />
+    <img style="max-width: 100%" class="lozad animate" data-src="{!! $block->image('image','flexible') !!}" alt="" />
 
     {!! $block->translatedInput('content') !!}
 </div>

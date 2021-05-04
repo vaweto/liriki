0383c619c614134bd85cafe1d8827aa271cd57f5
novoville <vagelis@novoville.com>
2021-05-04 13:31:05 +0300

    add cache clear command

--- routes/admin.php
+++ routes/admin.php
@@ -12,6 +12,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/clear-cache', function() {
      Artisan::call('cache:clear');
-    // return what you want
+    return redirect()->back()->with('status','Cache Cleared!');
+});
+
+Route::get('/clear-view', function() {
+    Artisan::call('view:clear');
+    return redirect()->back()->with('status','Views Cleared!');
 });
 

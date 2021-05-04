6f9b7bed63ff02bfd614dd24f33676a5d304fe9a
novoville <vagelis@novoville.com>
2021-05-04 13:26:49 +0300

    add cache clear command

--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 // Register Twill routes here eg.
@@ -8,4 +9,9 @@ use Illuminate\Support\Facades\Route;
  Route::module('blogs');
  Route::module('placemarks');
  Route::module('menus');
+
+Route::get('/clear-cache', function() {
+     Artisan::call('cache:clear');
+    // return what you want
+});
 

f074eb1ce51148e4b3530ffdef82982cf505b1dd
novoville <vagelis@novoville.com>
2021-06-05 18:22:35 +0300

    :hammer: last fixes from liriki

--- resources/views/content/blog.blade.php
+++ resources/views/content/blog.blade.php
@@ -11,14 +11,11 @@
 @section('content')
     <main>
         <div class="container">
-
-            <div class="flex-container">
-                <img width="100%" src="{!! $blog->image('cover','desktop') !!}" alt="{{$blog->title}}" />
-            </div>
             <div class="flex-container">
                 <div class="col-2-md">
                     <h2>{{$blog->title}}</h2>
 
+                    <img width="100%" src="{!! $blog->image('cover','desktop') !!}" alt="{{$blog->title}}" />
                     {!! $blog->content !!}
 
                     @if(!is_null($blog->file('pdf_file')))
@@ -28,12 +25,12 @@
                     @endif
                 </div>
                 <div class="col-1-md event-info">
-                    <div class="block">
-                        <h3>{{__('custom.date_created')}}</h3>
-                            <span class="current-post">{{$blog->created_at->day}}</span>
-                            <span class="separator">/</span>
-                            <span class="total-posts">{{$blog->created_at->month}}</span>
-                    </div>
+{{--                    <div class="block">--}}
+{{--                        <h3>{{__('custom.date_created')}}</h3>--}}
+{{--                            <span class="current-post">{{$blog->created_at->day}}</span>--}}
+{{--                            <span class="separator">/</span>--}}
+{{--                            <span class="total-posts">{{$blog->created_at->month}}</span>--}}
+{{--                    </div>--}}
                 </div>
             </div>
         </div>

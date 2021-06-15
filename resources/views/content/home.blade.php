ad7f8378e65a1960ccd0791c1e6e03ed8821d7bb
novoville <vagelis@novoville.com>
2021-06-15 10:32:21 +0300

    comment out all content

--- resources/views/content/home.blade.php
+++ resources/views/content/home.blade.php
@@ -19,129 +19,129 @@
         <div class="container">
             <div class="flex-container hero">
                 <div class="col-2-md align-right">
-                    <div style="margin-bottom: 20px">
-                        <video style="width: 100%; " id="vid" autoplay muted loop controls >
-                            <source src="{{asset('assets/public/LYRIKOS_NOTOS_VIDEO_FINAL.mp4')}}" type="video/mp4">
-                            Your browser does not support the video tag.
-                        </video>
-                    </div>
+{{--                    <div style="margin-bottom: 20px">--}}
+{{--                        <video style="width: 100%; " id="vid" autoplay muted loop controls >--}}
+{{--                            <source src="{{asset('assets/public/LYRIKOS_NOTOS_VIDEO_FINAL.mp4')}}" type="video/mp4">--}}
+{{--                            Your browser does not support the video tag.--}}
+{{--                        </video>--}}
+{{--                    </div>--}}
 
                     <p>
                         {!! $settings->byKey('home_text') !!}
                     </p>
                 </div>
             </div>
-            @foreach($categories  as $key => $category)
-                @php
-                    $categoryName = $category['category'];
-                    $categoryImage = $category['image'];
-                @endphp
-                @if($key === 0)
-                    <div class="flex-container">
-                        <div class="col-2-md">
-                            <h2 class="big-font">{{ $strHelper->strtoupper_no_accent(__('custom.'.$categoryName))}}</h2>
-                            <p>
-                            @if(! is_null($settings->byKey($categoryName.'_text')))
-                                    {!! $settings->byKey($categoryName.'_text') !!}
-                            @endif
-                            </p>
-                        </div>
-                        <div class="col-1-md events">
-                            <h3 >{{__('custom.scheduled_events')}}</h3>
-                            <ul>
-                                @forelse($category['events'] as $event)
-                                    <li>
-                                        <h4 class="big-font"><a href="/events/{{$event->slug}}">{{$strHelper->strtoupper_no_accent($event->title)}}</a></h4>
-                                        {!! $event->renderBlocks() !!}
-                                    </li>
-                                @empty
-                                    <li>
-                                        {{__('message.no_events_yet')}}
-                                    </li>
-                                @endforelse
-                            </ul>
-                        </div>
-                    </div>
-                @elseif($key === 1)
-                    <div class="flex-container ">
-                        <div class="col-2-md offset-left">
-                            <h2 class="big-font">{{ $strHelper->strtoupper_no_accent(__('custom.'.$categoryName))}}</h2>
-                            <p>
-                                @if(! is_null($settings->byKey($categoryName.'_text')))
-                                    {!! $settings->byKey($categoryName.'_text') !!}
-                                @endif
-                            </p>
-                        </div>
-                        <div class="col-1-md events">
-                            <h3>{{__('custom.scheduled_events')}}</h3>
-                            <ul>
-                                @forelse($category['events'] as $event)
-                                    <li>
-                                        <h4 class="big-font"><a href="/events/{{$event->slug}}">{{$strHelper->strtoupper_no_accent($event->title)}}</a></h4>
-                                        {!! $event->renderBlocks() !!}
-                                    </li>
-                                @empty
-                                    <li>
-                                        {{__('message.no_events_yet')}}
-                                    </li>
-                                @endforelse
-                            </ul>
-                        </div>
-                    </div>
-                @elseif($key === 2 || $key ===4)
-                    <div class="flex-container equal-cols">
-                        <div class="column">
-                            <h2 class="big-font">{{ $strHelper->strtoupper_no_accent(__('custom.'.$categoryName))}}</h2>
-                            <p>
-                                @if(! is_null($settings->byKey($categoryName.'_text')))
-                                    {!! $settings->byKey($categoryName.'_text') !!}
-                                @endif
-                            </p>
-                        </div>
-                        <div class="column events">
-                            <h3>{{__('custom.scheduled_events')}}</h3>
-                            <ul>
-                                @forelse($category['events'] as $event)
-                                    <li>
-                                        <h4 class="big-font"><a href="/events/{{$event->slug}}">{{$strHelper->strtoupper_no_accent($event->title)}}</a></h4>
-                                        {!! $event->renderBlocks() !!}
-                                    </li>
-                                @empty
-                                    <li>
-                                        {{__('message.no_events_yet')}}
-                                    </li>
-                                @endforelse
-                            </ul>
-                        </div>
-                    </div>
-                @elseif($key === 3)
-                    <div class="flex-container reversed">
-                        <div class="col-2-md offset-right">
-                            <h2 class="big-font">{{ $strHelper->strtoupper_no_accent(__('custom.'.$categoryName))}}</h2>
-                            <p>
-                                @if(! is_null($settings->byKey($categoryName.'_text')))
-                                    {!! $settings->byKey($categoryName.'_text') !!}
-                                @endif
-                            </p>
-                        </div>
-                        <div class="col-1-md events">
-                            <h3>{{__('custom.scheduled_events')}}</h3>
-                            <ul>
-                                @forelse($category['events'] as $event)
-                                    <li>
-                                        <h4 class="big-font"><a href="/events/{{$event->slug}}">{{$strHelper->strtoupper_no_accent($event->title)}}</a></h4>
-                                        {!! $event->renderBlocks() !!}
-                                    </li>
-                                @empty
-                                    <li>
-                                        {{__('message.no_events_yet')}}
-                                    </li>
-                                @endforelse
-                            </ul>
-                        </div>
-                    </div>
-                @endif
-            @endforeach
+{{--            @foreach($categories  as $key => $category)--}}
+{{--                @php--}}
+{{--                    $categoryName = $category['category'];--}}
+{{--                    $categoryImage = $category['image'];--}}
+{{--                @endphp--}}
+{{--                @if($key === 0)--}}
+{{--                    <div class="flex-container">--}}
+{{--                        <div class="col-2-md">--}}
+{{--                            <h2 class="big-font">{{ $strHelper->strtoupper_no_accent(__('custom.'.$categoryName))}}</h2>--}}
+{{--                            <p>--}}
+{{--                            @if(! is_null($settings->byKey($categoryName.'_text')))--}}
+{{--                                    {!! $settings->byKey($categoryName.'_text') !!}--}}
+{{--                            @endif--}}
+{{--                            </p>--}}
+{{--                        </div>--}}
+{{--                        <div class="col-1-md events">--}}
+{{--                            <h3 >{{__('custom.scheduled_events')}}</h3>--}}
+{{--                            <ul>--}}
+{{--                                @forelse($category['events'] as $event)--}}
+{{--                                    <li>--}}
+{{--                                        <h4 class="big-font"><a href="/events/{{$event->slug}}">{{$strHelper->strtoupper_no_accent($event->title)}}</a></h4>--}}
+{{--                                        {!! $event->renderBlocks() !!}--}}
+{{--                                    </li>--}}
+{{--                                @empty--}}
+{{--                                    <li>--}}
+{{--                                        {{__('message.no_events_yet')}}--}}
+{{--                                    </li>--}}
+{{--                                @endforelse--}}
+{{--                            </ul>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                @elseif($key === 1)--}}
+{{--                    <div class="flex-container ">--}}
+{{--                        <div class="col-2-md offset-left">--}}
+{{--                            <h2 class="big-font">{{ $strHelper->strtoupper_no_accent(__('custom.'.$categoryName))}}</h2>--}}
+{{--                            <p>--}}
+{{--                                @if(! is_null($settings->byKey($categoryName.'_text')))--}}
+{{--                                    {!! $settings->byKey($categoryName.'_text') !!}--}}
+{{--                                @endif--}}
+{{--                            </p>--}}
+{{--                        </div>--}}
+{{--                        <div class="col-1-md events">--}}
+{{--                            <h3>{{__('custom.scheduled_events')}}</h3>--}}
+{{--                            <ul>--}}
+{{--                                @forelse($category['events'] as $event)--}}
+{{--                                    <li>--}}
+{{--                                        <h4 class="big-font"><a href="/events/{{$event->slug}}">{{$strHelper->strtoupper_no_accent($event->title)}}</a></h4>--}}
+{{--                                        {!! $event->renderBlocks() !!}--}}
+{{--                                    </li>--}}
+{{--                                @empty--}}
+{{--                                    <li>--}}
+{{--                                        {{__('message.no_events_yet')}}--}}
+{{--                                    </li>--}}
+{{--                                @endforelse--}}
+{{--                            </ul>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                @elseif($key === 2 || $key ===4)--}}
+{{--                    <div class="flex-container equal-cols">--}}
+{{--                        <div class="column">--}}
+{{--                            <h2 class="big-font">{{ $strHelper->strtoupper_no_accent(__('custom.'.$categoryName))}}</h2>--}}
+{{--                            <p>--}}
+{{--                                @if(! is_null($settings->byKey($categoryName.'_text')))--}}
+{{--                                    {!! $settings->byKey($categoryName.'_text') !!}--}}
+{{--                                @endif--}}
+{{--                            </p>--}}
+{{--                        </div>--}}
+{{--                        <div class="column events">--}}
+{{--                            <h3>{{__('custom.scheduled_events')}}</h3>--}}
+{{--                            <ul>--}}
+{{--                                @forelse($category['events'] as $event)--}}
+{{--                                    <li>--}}
+{{--                                        <h4 class="big-font"><a href="/events/{{$event->slug}}">{{$strHelper->strtoupper_no_accent($event->title)}}</a></h4>--}}
+{{--                                        {!! $event->renderBlocks() !!}--}}
+{{--                                    </li>--}}
+{{--                                @empty--}}
+{{--                                    <li>--}}
+{{--                                        {{__('message.no_events_yet')}}--}}
+{{--                                    </li>--}}
+{{--                                @endforelse--}}
+{{--                            </ul>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                @elseif($key === 3)--}}
+{{--                    <div class="flex-container reversed">--}}
+{{--                        <div class="col-2-md offset-right">--}}
+{{--                            <h2 class="big-font">{{ $strHelper->strtoupper_no_accent(__('custom.'.$categoryName))}}</h2>--}}
+{{--                            <p>--}}
+{{--                                @if(! is_null($settings->byKey($categoryName.'_text')))--}}
+{{--                                    {!! $settings->byKey($categoryName.'_text') !!}--}}
+{{--                                @endif--}}
+{{--                            </p>--}}
+{{--                        </div>--}}
+{{--                        <div class="col-1-md events">--}}
+{{--                            <h3>{{__('custom.scheduled_events')}}</h3>--}}
+{{--                            <ul>--}}
+{{--                                @forelse($category['events'] as $event)--}}
+{{--                                    <li>--}}
+{{--                                        <h4 class="big-font"><a href="/events/{{$event->slug}}">{{$strHelper->strtoupper_no_accent($event->title)}}</a></h4>--}}
+{{--                                        {!! $event->renderBlocks() !!}--}}
+{{--                                    </li>--}}
+{{--                                @empty--}}
+{{--                                    <li>--}}
+{{--                                        {{__('message.no_events_yet')}}--}}
+{{--                                    </li>--}}
+{{--                                @endforelse--}}
+{{--                            </ul>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                @endif--}}
+{{--            @endforeach--}}
         </div>
     </main>
 @stop

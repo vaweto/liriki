358cb716602d6bb5f1e6690264be302e7d725998
vaweto <vagelisbisbikis@gmail.com>
2022-05-16 21:33:11 +0300

    register simple paginator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Paginator::defaultSimpleView('view-name');
+        Paginator::useBootstrap();
     }
 }

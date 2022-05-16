dc343ed03a5a7c4acfb1b9866ae6c9ce518c001c
vaweto <vagelisbisbikis@gmail.com>
2022-05-16 22:13:59 +0300

    bootstrap pagination style fixes

--- resources/views/vendor/pagination/simple-bootstrap-4.blade.php
+++ resources/views/vendor/pagination/simple-bootstrap-4.blade.php
@@ -1,24 +1,31 @@
 @if ($paginator->hasPages())
-    <nav>
-        <ul class="pagination">
+    <nav style="
+        display: block;
+        width: 100%;
+    ">
+        <ul class="pagination" style="
+            text-align: center;
+            display: inline-block;
+            width: 100%;
+        ">
             {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
-                <li class="page-item disabled" aria-disabled="true">
+                <li style="display: inline-block;" class="page-item disabled" aria-disabled="true">
                     <span class="page-link">@lang('pagination.previous')</span>
                 </li>
             @else
-                <li class="page-item">
+                <li style="display: inline-block;" class="page-item">
                     <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev">@lang('pagination.previous')</a>
                 </li>
             @endif
 
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
-                <li class="page-item">
+                <li style="display: inline-block;" class="page-item">
                     <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next">@lang('pagination.next')</a>
                 </li>
             @else
-                <li class="page-item disabled" aria-disabled="true">
+                <li style="display: inline-block;" class="page-item disabled" aria-disabled="true">
                     <span class="page-link">@lang('pagination.next')</span>
                 </li>
             @endif

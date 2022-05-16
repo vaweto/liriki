35357e37a4a6e84ed1688cc25a05a0eb243d6b0e
vaweto <vagelisbisbikis@gmail.com>
2022-05-16 22:19:04 +0300

    style paginator default

--- resources/views/vendor/pagination/simple-default.blade.php
+++ resources/views/vendor/pagination/simple-default.blade.php
@@ -1,18 +1,25 @@
 @if ($paginator->hasPages())
-    <nav>
-        <ul class="pagination">
+    <nav style="
+        display: block;
+        width: 100%;
+    ">
+        <ul class="pagination" style="
+            text-align: center;
+            display: inline-block;
+            width: 100%;
+        ">
             {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
-                <li class="disabled" aria-disabled="true"><span>@lang('pagination.previous')</span></li>
+                <li style="display: inline-block;"class="disabled" aria-disabled="true"><span>@lang('pagination.previous')</span></li>
             @else
-                <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev">@lang('pagination.previous')</a></li>
+                <li style="display: inline-block;"><a href="{{ $paginator->previousPageUrl() }}" rel="prev">@lang('pagination.previous')</a></li>
             @endif
 
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
-                <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">@lang('pagination.next')</a></li>
+                <li style="display: inline-block;"><a href="{{ $paginator->nextPageUrl() }}" rel="next">@lang('pagination.next')</a></li>
             @else
-                <li class="disabled" aria-disabled="true"><span>@lang('pagination.next')</span></li>
+                <li style="display: inline-block;" class="disabled" aria-disabled="true"><span>@lang('pagination.next')</span></li>
             @endif
         </ul>
     </nav>
